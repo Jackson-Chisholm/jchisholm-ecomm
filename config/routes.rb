d9f389f05cc276a09568d82e1b67716f11a39c88
Jackson-Chisholm <jchisholm3@rrc.ca>
2026-07-15 23:47:15 -0500

    github upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "items/index"
+  get "items/show"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  root "items#index"
+  resources :items
 
   # Defines the root path route ("/")
   # root "posts#index"
