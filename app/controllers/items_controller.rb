60f057a3f4a9aee4fe21be40468a97d51dd18eda
Jackson Chisholm <jchisholm3@rrc.ca>
2026-07-16 22:57:45 -0500

    First Milestone Tasks Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,24 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    if params[:query].present?
+    @items = Item.where(
+      "name LIKE :term OR description LIKE :term",
+      term: "%#{params[:query]}%"
+    ).page(params[:page]).per(5)
+
+    if params[:limit_to_franchise] == "1" && params[:set].present?
+      @items = @items.where(franchise: params[:set]).where("name LIKE :term OR description LIKE :term",
+      term: "%#{params[:query]}%").page(params[:page]).per(5)
+    end
+
+  elsif params[:franchise].present?
+    @items = Item.where(franchise: params[:franchise]).page(params[:page]).per(5)
+
+  else
+    @items = Item.all.page(params[:page]).per(5)
+  end
+
+  @franchises = Item.distinct.pluck(:franchise)
   end
 
   def show
