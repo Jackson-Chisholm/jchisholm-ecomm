56a3fdc316aa069d7b284d6596d0d8182d2337d9
Jackson-Chisholm <jchisholm3@rrc.ca>
2026-08-04 20:30:58 -0500

    Base of Testing Branch

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,13 @@ class OrdersController < ApplicationController
   end
 
   def new
+    before_action :authenticate_customer!
+
+    @order = Order.new
+    @cart = session[:cart] || {}
+    @items = Item.where(id: @cart.keys)
+  end
+
+  def create
   end
 end
