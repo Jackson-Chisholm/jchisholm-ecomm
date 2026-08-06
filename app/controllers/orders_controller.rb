8b94e71dd7b33e48714a6165b953b75d0df5ca77
Jackson-Chisholm <jchisholm3@rrc.ca>
2026-08-05 21:15:59 -0500

    Order Complete

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,5 +11,17 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      session[:cart] = {}
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+    def order_params
+      params.expect(order: [ :items, :order_total, :customer_id, :status ])
+    end
 end
