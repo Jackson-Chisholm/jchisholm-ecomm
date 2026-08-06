8953120d6be767ca3d498bebaabe952a1bf3f56d
Jackson-Chisholm <jchisholm3@rrc.ca>
2026-08-05 20:24:35 -0500

    Form now hidden

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_31_043652) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_05_043512) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.integer "author_id"
     t.string "author_type"
@@ -97,6 +97,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_31_043652) do
     t.integer "customer_id", null: false
     t.string "items"
     t.date "order_date"
+    t.float "order_total"
     t.string "status"
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
